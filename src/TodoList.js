ff5fd2c1d25afddc97bcbf1b95acdca3f3f877ac
Peter Darinzo <birel44@yahoo.com>
2022-01-13 09:20:45 -0800

    fix toggle, add, and delete

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Todo from "./Todo"
 import NewTodoForm from "./NewTodoForm";
 import { v4 as uuid } from "uuid";
@@ -9,44 +9,32 @@ const TodoList = () => {
 
   const [tasks, setTasks] = useState([]);
 
-  // get tasks from local storage upon load
-  window.addEventListener('DOMContentLoaded', (e) => {
+  useEffect(() => {
     if (localStorage.getItem("tasks") === null) {
       localStorage.setItem("tasks", JSON.stringify(tasks));
     }
     setTasks(() => JSON.parse(localStorage.getItem("tasks")));
-  });
+  }, []);
+
 
   const addTask = (newTask) => {
-    setTasks((tasks) => {
-      const newTasks = [...tasks, { ...newTask, id: uuid() }];
-      localStorage.setItem("tasks", JSON.stringify(newTasks));
-      return newTasks;
-    });
+    const newTasks = [...tasks, { ...newTask, id: uuid() }]
+    localStorage.setItem("tasks", JSON.stringify(newTasks));
+    setTasks(tasks => newTasks);
   }
 
   const toggleComplete = (id) => {
-    let task = {};
-    for (let t of tasks) {
-      if (t.id === id) {
-        task = t;
-      }
-    }
-    deleteTask(id);
-    task.completed === false ? task.completed = true : task.completed = false;
-    setTasks((tasks) => {
-      const newTasks = [...tasks, { ...task }];
-      localStorage.setItem("tasks", JSON.stringify(newTasks));
-      return newTasks
-    });
+    let newTasks = [...tasks];
+    let taskIndex = newTasks.findIndex(task => task.id === id);
+    newTasks[taskIndex].completed === false ? newTasks[taskIndex].completed = true : newTasks[taskIndex].completed = false;
+    localStorage.setItem("tasks", JSON.stringify(newTasks));
+    setTasks(tasks => newTasks);
   }
 
   const deleteTask = (id) => {
-    setTasks((tasks) => {
-      const newTasks = tasks.filter(task => task.id !== id);
-      localStorage.setItem("tasks", JSON.stringify(newTasks));
-      return newTasks;
-    });
+    const newTasks = tasks.filter(task => task.id !== id);
+    localStorage.setItem("tasks", JSON.stringify(newTasks));
+    setTasks(tasks => newTasks);
   }
 
   return (
